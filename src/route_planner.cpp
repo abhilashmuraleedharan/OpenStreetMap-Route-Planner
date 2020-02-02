f7c32aecb4cb5882021070f007c61f3be6cc491c
Abhilash Muraleedharan <amuraleedharan13@gmail.com>
2020-02-02 14:51:27 +0530

    Implements ConstructFinalPath method
    Return the final path found by the A* Search

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -51,25 +51,30 @@ RouteModel::Node *RoutePlanner::NextNode() {
     return l_sum_node;
 }
 
-
-// TODO 6: Complete the ConstructFinalPath method to return the final path found from your A* search.
-// Tips:
-// - This method should take the current (final) node as an argument and iteratively follow the 
-//   chain of parents of nodes until the starting node is found.
-// - For each node in the chain, add the distance from the node to its parent to the distance variable.
-// - The returned vector should be in the correct order: the start node should be the first element
-//   of the vector, the end node should be the last element.
-
 std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
     // Create path_found vector
     distance = 0.0f;
     std::vector<RouteModel::Node> path_found;
 
-    // TODO: Implement your solution here.
-
-    distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
+    /*
+     * Iteratively follow the chain of parents of nodes until the starting node is found.
+     * For each node in the chain, add the distance from the node to its parent to the distance variable.
+     * The returned vector should be in the correct order: the start node should be the first element
+     * of the vector, the end node should be the last element.
+     */
+    RouteModel::Node *temp_node = current_node;
+    while(temp_node != start_node) {
+        path_found.push_back(*temp_node);
+        distance += temp_node->distance(*temp_node->parent);
+        temp_node = temp_node->parent;
+    }
+    // Add the start_node to the path_found vector
+    path_found.push_back(*temp_node);
+    // Reverse the order of elements in the vector
+    std::reverse(path_found.begin(), path_found.end());
+    // Multiply the distance by the scale of the map to get meters.
+    distance *= m_Model.MetricScale(); 
     return path_found;
-
 }
 
 
